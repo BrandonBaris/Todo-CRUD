40701aa8de330109204836103474c87723920e1b
brandonbaris <brandonbaris@gmail.com>
2015-03-03 15:49:15 -1000

    adds complete counter

--- public/js/todo.js
+++ public/js/todo.js
@@ -1,58 +1,39 @@
 $(function(){
+
+function countChecks() {
   var checked = 0;
   var totalItem = 0;
   var checkDifference = 0;
-function countChecks() {
-
   checked = $("input#check_stat:checked").length;
   totalItem = $("input").length;
   checkDifference = totalItem - checked;
   console.log('checked',checked);
   console.log('totalItem',totalItem);
   console.log('checkDifference',checkDifference);
-  return checked,totalItem,checkDifference;
+  var buildCounter = document.getElementById('counter').innerHTML = " CHECKED " + checked + " INCOMPLETE: " + checkDifference;
+  return buildCounter;
 }
 
 $("input#check_stat").on("change", function (){
   var todoId = $(this).val();
-  // console.log($(this).prop('checked'));
   countChecks();
 
   if ($(this).prop('checked')){
-    $.ajax(
-      '/todos/' + todoId + '/complete',{
-      
+    $.ajax({
+      url: '/todos/' + todoId + '/complete',
       type : 'PUT'
     });
-    } else {
-    $.ajax(
-      '/todos/' + todoId + '/incomplete',{
+  } else {
+    $.ajax({
+      url: '/todos/' + todoId + '/incomplete',
       type : 'PUT'
     });
   }
-  
-  // var checked = 0; 
-  // var totalCheckBox = 0;
-  // $("input#check_stat").each(function() {
-  //   if ($(this).is(':checked')){
-  //     checked++;
-  //   }
-  //   console.log('checked',checked);
-  //   return checked;
-  // });
-  // $("input#check_stat").each(function() {
-  //     totalCheckBox++;
-    
-  //   console.log('total',totalCheckBox);
-  //   return totalCheckBox;
-  // });
-  // var checkDifference = totalCheckBox - checked;
-  // console.log('notchecked',checkDifference);
 });
 
 $( window ).load(function() {
-  countChecks();
+  countChecks(function() {
+  });
 });
-
 
 }); //end closurex
